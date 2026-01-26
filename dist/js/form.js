e0d5338a1ef13fbd2b4f488f3f24e2fb5726e881
trueesel <merz.jan@gmx.de>
2026-01-26 10:58:31 +0100

    - added ability to add an email and a matching form.

--- dist/js/form.js
+++ dist/js/form.js
@@ -82,10 +82,97 @@ function same_as_ec(checked)  {
     }
 }
 
+function getInputValue(id)  {
+    const value = document.getElementById(id)?.value.trim();
+    return value || "";
+}
+
+function setPersonInfo(personIndex)  {
+    let person = {}
+
+    //set values of person
+    person.first_name = getInputValue("first-name");
+    person.last_name = getInputValue("last-name");
+    person.birthdate = getInputValue("birthdate");
+    person.address = getInputValue("address");
+    person.postal_code = getInputValue("postal-code");
+    person.town = getInputValue("town");
+    person.mobile = getInputValue("mobile");
+    person.email = getInputValue("email");
+    person.passport_number = getInputValue("passport-number");
+
+    let gender_arr = document.getElementsByName("flexRadioGender");
+    person.gender = "";
+    for (let i = 0; i < gender_arr.length; i++) {
+        if (gender_arr[i].checked) {
+            person.gender = gender_arr[i].value;
+            break;
+        }
+    }
+
+    person.disability = getInputValue("disability") || "/";
+    person.allergies = getInputValue("allergies") || "/";
+
+    // Info of emergency contact
+    person.first_name_ec = getInputValue("first-name-ec");
+    person.last_name_ec = getInputValue("last-name-ec");
+    person.address_ec = getInputValue("address-ec");
+    person.postal_code_ec = getInputValue("postal-code-ec");
+    person.town_ec = getInputValue("town-ec");
+    person.mobile_ec = getInputValue("mobile-ec");
+    person.email_ec = getInputValue("email-ec");
+
+    let gender_ec_arr = document.getElementsByName("flexRadioGenderEmContact");
+    person.gender_ec = "";
+    for (let i = 0; i < gender_ec_arr.length; i++) {
+        if (gender_ec_arr[i].checked) {
+            person.gender_ec = gender_ec_arr[i].value;
+            break;
+        }
+    }
+
+    // Info of legal guardian
+    person.first_name_lg = getInputValue("first-name-lg");
+    person.last_name_lg = getInputValue("last-name-lg");
+    person.address_lg = getInputValue("address-lg");
+    person.postal_code_lg = getInputValue("postal-code-lg");
+    person.town_lg = getInputValue("town-lg");
+    person.mobile_lg = getInputValue("mobile-lg");
+    person.email_lg = getInputValue("email-lg");
+
+    let gender_lg_arr = document.getElementsByName("flexRadioGenderLegalGu");
+    person.gender_lg = "";
+    for (let i = 0; i < gender_lg_arr.length; i++) {
+        if (gender_lg_arr[i].checked) {
+            person.gender_lg = gender_lg_arr[i].value;
+            break;
+        }
+    }
+
+    person.wishes = getInputValue("wishes") || "/";
+
+    //save person
+    let formInfo = JSON.parse(localStorage.getItem("formInfo"));
+    formInfo[personIndex] = person;
+    localStorage.setItem("formInfo", JSON.stringify(formInfo));
+}
+
+//TODO unsure if I need this
+function getPersonInfo(personIndex) {
+
+}
+
 //TODO this is an awful name. We're not writing Java code and this is not an subclass that extends a class, which implements another class. And the name is not specific despite being that long. It has no business being this long. My brain hurts.
 function initializeAmountSpecificThings()   {
     //for debugging purposes
     localStorage.setItem("numberOfPersons", JSON.stringify(3));
+
+    //TODO initialize this array when clicking whichever button lets you get to the form with the amount of persons selected doing it here is just for debugging purposes --> just copy this codeblock in a method thats called there and it should be fine
+    localStorage.setItem("formInfo", JSON.stringify([]));
+    for (let i = 0; i < JSON.parse(localStorage.getItem("numberOfPersons")); i++) {
+        //every empty dict is a placeholder for a persons form
+        setPersonInfo(i)
+    }
 
     sessionStorage.setItem("currentPerson", JSON.stringify(1));
     //if its just one person, hide the previous/next button and let the name stay form
@@ -99,18 +186,62 @@ function initializeAmountSpecificThings()   {
     }
 }
 
-function saveCurrentPerson(currentPerson)   {
+function loadCurrentPerson(personIndex) {
+    let person = JSON.parse(localStorage.getItem("formInfo"))[personIndex];
 
-}
+    document.getElementById("first-name").value = person.first_name;
+    document.getElementById("last-name").value = person.last_name;
+    document.getElementById("birthdate").value = person.birthdate;
+    document.getElementById("address").value = person.address;
+    document.getElementById("postal-code").value = person.postal_code;
+    document.getElementById("town").value = person.town;
+    document.getElementById("mobile").value = person.mobile;
+    document.getElementById("email").value = person.email;
+    document.getElementById("passport-number").value = person.passport_number;
 
-function loadCurrentPerson(currentPerson) {
+    let genderArr = document.getElementsByName("flexRadioGender");
+    for (let i = 0; i < genderArr.length; i++) {
+        genderArr[i].checked = genderArr[i].value === person.gender;
+    }
 
+    document.getElementById("disability").value = person.disability !== "/" ? person.disability : "";
+    document.getElementById("allergies").value = person.allergies !== "/" ? person.allergies : "";
+
+    // Info of emergency contact
+    document.getElementById("first-name-ec").value = person.first_name_ec;
+    document.getElementById("last-name-ec").value = person.last_name_ec;
+    document.getElementById("address-ec").value = person.address_ec;
+    document.getElementById("postal-code-ec").value = person.postal_code_ec;
+    document.getElementById("town-ec").value = person.town_ec;
+    document.getElementById("mobile-ec").value = person.mobile_ec;
+    document.getElementById("email-ec").value = person.email_ec;
+
+    let genderEcArr = document.getElementsByName("flexRadioGenderEmContact");
+    for (let i = 0; i < genderEcArr.length; i++) {
+        genderEcArr[i].checked = genderEcArr[i].value === person.gender_ec;
+    }
+
+    // Info of legal guardian
+    document.getElementById("first-name-lg").value = person.first_name_lg;
+    document.getElementById("last-name-lg").value = person.last_name_lg;
+    document.getElementById("address-lg").value = person.address_lg;
+    document.getElementById("postal-code-lg").value = person.postal_code_lg;
+    document.getElementById("town-lg").value = person.town_lg;
+    document.getElementById("mobile-lg").value = person.mobile_lg;
+    document.getElementById("email-lg").value = person.email_lg;
+
+    let genderLgArr = document.getElementsByName("flexRadioGenderLegalGu");
+    for (let i = 0; i < genderLgArr.length; i++) {
+        genderLgArr[i].checked = genderLgArr[i].value === person.gender_lg;
+    }
+
+    document.getElementById("wishes").value = person.wishes !== "/" ? person.wishes : "";
 }
 
 function loadDifferentForm(nextForm)    {
     let currentPerson = JSON.parse(sessionStorage.getItem("currentPerson"));
 
-    saveCurrentPerson(currentPerson);
+    setPersonInfo(currentPerson-1);
     if (nextForm) {
         currentPerson += 1
         sessionStorage.setItem("currentPerson", JSON.stringify(currentPerson));
@@ -126,7 +257,7 @@ function loadDifferentForm(nextForm)    {
             document.getElementById("previousPerson").setAttribute('disabled', 'disabled');
         }
     }
-    loadCurrentPerson(currentPerson);
+    loadCurrentPerson(currentPerson-1);
     document.getElementById("headerForm").innerHTML = "Form for Person " + currentPerson;
 }
 
@@ -161,70 +292,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function process_form() {
     //info of person that goes on the trip
-    let first_name = document.getElementById("first-name").value.trim();
-    let last_name = document.getElementById("last-name").value.trim();
-    let birthdate = document.getElementById("birthdate").value.trim();
-    let address = document.getElementById("address").value.trim();
-    let postal_code = document.getElementById("postal-code").value.trim();
-    let town = document.getElementById("town").value.trim();
-    let mobile = document.getElementById("mobile").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let passport_number = document.getElementById("passport-number").value.trim();
-    let gender = ""
-    let gender_arr = document.getElementsByName("flexRadioGender");
-    for (let i = 0; i < gender_arr.length; i++) {
-        if (gender_arr[i].checked) {
-            gender = gender_arr[i].value;
-            break;
-        }
-    }
-    let disability = document.getElementById("disability").value.trim();
-    if (disability === "") {
-        disability = "/";
-    }
-    let allergies = document.getElementById("allergies").value.trim();
-    if (allergies === "") {
-        allergies = "/";
-    }
 
-    //info of emergency contact
-    let first_name_ec = document.getElementById("first-name-ec").value.trim();
-    let last_name_ec = document.getElementById("last-name-ec").value.trim();
-    let address_ec = document.getElementById("address-ec").value.trim();
-    let postal_code_ec = document.getElementById("postal-code-ec").value.trim();
-    let town_ec = document.getElementById("town-ec").value.trim();
-    let mobile_ec = document.getElementById("mobile-ec").value.trim();
-    let email_ec = document.getElementById("email-ec").value.trim();
-    let gender_ec = "";
-    let gender_ec_arr = document.getElementsByName("flexRadioGenderEmContact");
-    for (let i = 0; i < gender_ec_arr.length; i++) {
-        if (gender_ec_arr[i].checked) {
-            gender_ec = gender_ec_arr[i].value;
-            break;
-        }
-    }
-
-    //info of legal guardian
-    let first_name_lg = document.getElementById("first-name-lg").value.trim();
-    let last_name_lg = document.getElementById("last-name-lg").value.trim();
-    let address_lg = document.getElementById("address-lg").value.trim();
-    let postal_code_lg = document.getElementById("postal-code-lg").value.trim();
-    let town_lg = document.getElementById("town-lg").value.trim();
-    let mobile_lg = document.getElementById("mobile-lg").value.trim();
-    let email_lg = document.getElementById("email-lg").value.trim();
-    let gender_lg = "";
-    let gender_lg_arr = document.getElementsByName("flexRadioGenderLegalGu");
-    for (let i = 0; i < gender_lg_arr.length; i++) {
-        if (gender_lg_arr[i].checked) {
-            gender_lg = gender_lg_arr[i].value;
-            break;
-        }
-    }
-
-    let wishes = document.getElementById("wishes").value.trim();
-    if (wishes === "") {
-        wishes = "/";
-    }
 
     sendPDF(first_name, last_name, birthdate, address, postal_code, town, mobile, email, passport_number, gender, disability, allergies, first_name_ec, last_name_ec,
         address_ec, postal_code_ec, town_ec, mobile_ec, email_ec, gender_ec, first_name_lg, last_name_lg, address_lg, postal_code_lg, town_lg, mobile_lg, email_lg, gender_lg, wishes);
