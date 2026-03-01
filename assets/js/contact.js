418bf30c30d4bd678d5eb2882336e1dadbcf1632
Laura van Uffelt <laura.vanuffelt@icloud.com>
2026-03-01 13:16:33 +0100

    integrate some js

---
+++ assets/js/contact.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const submitButton = document.getElementById("submit");
+
+    submitButton.addEventListener("click", () => {
+        const form = document.querySelector('.needs-validation');
+
+        form.classList.add('was-validated');
+
+        if(form.checkValidity())    {
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const phone = document.getElementById("phone").value;
+            const message = document.getElementById("message").value;
+            const formContent = JSON.stringify({name, email, phone, message});
+            sendContactMail(formContent, form);
+        }
+    });
+});
+
+function sendContactMail(formContent, form) {
+    fetch('send_email_contact_page.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: formContent
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Your message has been sent!');
+                form.reset();
+                form.classList.remove('was-validated');
+
+            } else {
+                alert(`Error: ${data.message}`);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An issue occurred while sending the contact form.');
+        });
+}
---
+++ assets/js/vanilla.js
@@ -0,0 +1,133 @@
+
+
+
+function round(val, amount) {
+    return Math.round(val * Math.pow(10, amount)) / Math.pow(10, amount);
+}
+
+//renders handlebars template objects
+function render(template_obj, dest_obj, data) {
+    const template = Handlebars.compile(template_obj.innerHTML);
+    dest_obj.innerHTML = template(data)
+}
+
+//Shared Logic for the senior and student sites
+
+function updatePriceFields(fields, texts, prices) {
+    for (let i = 0; i < fields.length; i++) {
+        fields[i].textContent = texts[i] + `\u00A3${prices[i].toFixed(2)}`;
+    }
+}
+
+function changeNumberParticipants(peopleCountField, btn, oppositeBtn, isMinusBtn, price=0, numberDisableBtn = 0)    {
+    let countBtn = peopleCountField.textContent;
+    if (isMinusBtn) {
+        countBtn --;
+        peopleCountField.textContent = countBtn;
+        btn.disabled = countBtn === numberDisableBtn;
+    } else {
+        countBtn ++;
+        peopleCountField.textContent = countBtn;
+        btn.disabled = countBtn === count;
+    }
+    oppositeBtn.disabled = false;
+
+    if (price !== 0)    {
+        calc_Activity_Price(price)
+    }
+}
+
+function enableButtons(buttons) {
+
+    buttons.forEach(button => {button.disabled = false;})
+}
+
+function calc_Activity_Price(price) {
+    activityPrice += price;
+    updatePriceFieldCall()
+}
+
+function addExtrasCost(price) {
+    totalExtrasCost += price;
+    calculatePrice();
+}
+
+function checkEnable(plusButton)    {
+    plusButton.disabled = false;
+}
+
+function checkDisable(plusButtonFirst, plusButtonSecond, peopleCountFirst, peopleCountSecond)    {
+    if (parseInt(peopleCountFirst.textContent) + parseInt(peopleCountSecond.textContent) === count) {
+        plusButtonFirst.disabled = true;
+        plusButtonSecond.disabled = true;
+    }
+}
+
+function saveInformation(date)  {
+    //p tag with optional activities
+    const optionalActivities = document.querySelectorAll('.optional-activity');
+
+    let i = 0;
+    let activities = [];
+
+    optionalActivities.forEach((element, index) => {
+        let peopleCountAct = parseInt(peopleCountArr[index].textContent)
+
+        if (peopleCountAct !== 0)    {
+            let activityDesc = "";
+
+            if (element.textContent.startsWith('Optional: ')) {
+                activityDesc = element.textContent.replace('Optional: ', '');
+            } else {
+                activityDesc = element.textContent;
+            }
+
+            let activity = {}
+            activity.description = activityDesc;
+            activity.amountPeople = peopleCountAct;
+            activities[i] = activity;
+
+            i += 1;
+        }
+    });
+
+    const extras = document.querySelectorAll('.extras');
+
+    i = 0;
+    let extraArr = [];
+
+    extras.forEach((element, index) => {
+        let extrasAct = parseInt(extrasCountArr[index].textContent)
+
+        if (extrasAct !== 0)    {
+            let extraDesc = "";
+
+            if (element.textContent.startsWith('Optional: ')) {
+                extraDesc = element.textContent.replace('Optional: ', '');
+            } else {
+                extraDesc = element.textContent;
+            }
+
+            let extra = {}
+            extra.description = extraDesc;
+            extra.amountPeople = extrasAct;
+            extraArr[i] = extra;
+            i += 1;
+        }
+    });
+
+    let other_information = {}
+    other_information.subTotal = subTotal;
+    other_information.total = total;
+    other_information.amountPeople = parseInt(peopleCount.textContent);
+    other_information.tripType = JSON.parse(localStorage.getItem("tripName"));
+    other_information.date = date;
+
+    let information = {};
+    information.otherInformation = other_information;
+    information.activityInfo = activities;
+    information.extraInfo = extraArr;
+
+    localStorage.setItem("tripInfo", JSON.stringify(information));
+}
+
